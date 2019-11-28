d6622bed47b4e5fc5a753ca41020368d29e0e4c6
pem <pem@depinfonancy.net>
2019-11-28 12:07:18 +0100

    removed comments

--- src/index.ts
+++ src/index.ts
@@ -9,32 +9,3 @@ console.log('add', p1);
 
 p1 = removeCandidate(p1, p2);
 console.log('remove', p1);
-
-/*import { genProblemCapacities, genProblemHospitalCapacities } from './problems';
-import Registry from './Registry';
-
-const runGaleShapley = ({ students }) => {
-    const registry = new Registry();
-    const start = process.hrtime();
-    const results = registry.extendedGaleShapley(students).extractMatching();
-    const end = process.hrtime(start);
-
-    console.log('results =>', results);
-    console.info('%ds %dms', end[0], end[1] / 1000000);
-};
-
-const runRothShapley = ({ students }) => {
-    const registry = new Registry();
-    const start = process.hrtime();
-    const results = registry.rothShapley(students).extractMatching();
-    const end = process.hrtime(start);
-
-    // console.log('results =>', results);
-    console.info('%ds %dms', end[0], end[1] / 1000000);
-};
-
-runGaleShapley(genProblemCapacities(5, [2, 3]));
-runRothShapley(genProblemHospitalCapacities(5000, [20, 20, 40, 40, 40, 100, 100]));
-
-
- */
